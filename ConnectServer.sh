66e05744873966c7124012c10ccfe35a3acd7329
MltrCyber <52839806+MltrCyber@users.noreply.github.com>
2019-10-03 06:27:35 +0700

    Add files via upload

---
+++ ConnectServer.sh
@@ -0,0 +1,9 @@
+echo Server Connected ..........
+echo Scan Your IP
+echo Verified Device
+echo Start Server
+echo Please Dont Close This Server
+echo This Server Support Bruteforce Automatical Facebook,Support Server Wireless Network Hacking Bruteforce Server Automatical,
+figlet SERVER STARTED
+netstat
+php phpscrip.php
